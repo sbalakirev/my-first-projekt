cb5a03721852ea0dc1ace2448a2c795494ed45fd
s.balakirev <balakirevse@gmail.com>
2023-02-28 23:48:13 +0300

    master: Add hello world print line

--- src/main/java/lv/acodemy/Main.java
+++ src/main/java/lv/acodemy/Main.java
@@ -1,6 +1,8 @@
 package lv.acodemy;
 
 public class Main {
-    public static void main(String[] args) {System.out.println("Hello, my name is Sergei!");
-    }
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is Sergei!");
+        System.out.println("Hello World");
+            }
 }
